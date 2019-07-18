687a6651fe1d9cb6a15d17d57d8b1d436f107f01
Lee heon gyu <frtt0608@naver.com>
2019-07-18 14:50:10 +0900

    190718|spring boot + MariaDB + Vue.js 연동작업, 스켈레톤 구성

---
+++ SSAFYhashtag/src/main/java/com/ssafy/hashtag/SsafYhashtagApplication.java
@@ -0,0 +1,12 @@
+package com.ssafy.hashtag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafYhashtagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsafYhashtagApplication.class, args);
+	}
+}
