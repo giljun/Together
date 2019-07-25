b1b37d15ca86458cbf2da7b900f71b7621936533
Lee heon gyu <frtt0608@naver.com>
2019-07-25 17:55:56 +0900

    190725|Table에 관한 Dto 생성

--- SSAFYhashtag/src/main/java/com/ssafy/hashtag/db/service/TestService.java
+++ SSAFYhashtag/src/main/java/com/ssafy/hashtag/db/service/TestService.java
@@ -29,49 +29,53 @@ public class TestService {
         System.out.println("****************add locationBasedList**********************");
 
         List<Map<String, Object>> lists = new ArrayList<Map<String, Object>>();
+        
+        int[] codes = {1,2,3,4,5,6,7,8,31,32,33,34,35,36,37,38,39};
 
-        lists = insertInfo();
+        for(int j=0; j < codes.length; j++) {
+            lists = insertInfo(codes[j]);
 
-        PostDto postdto = new PostDto();
-        String non_image = "https://images.unsplash.com/photo-1536765659537-ac6b544ea73b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60";
+            PostDto postdto = new PostDto();
+            // String non_image = "https://images.unsplash.com/photo-1536765659537-ac6b544ea73b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60";
 
-        for (int i = 0; i < lists.size(); i++) {
-            System.out.println("****************add in for**********************");
+            for (int i = 0; i < lists.size(); i++) {
+                // System.out.println("****************add in for**********************");
 
-            Map map = lists.get(i);
-            postdto.setAddress((String) map.get("address"));
+                Map map = lists.get(i);
+                postdto.setAddress((String) map.get("address"));
 
-            if(map.get("image") != null) {
-                postdto.setImage((String) map.get("image"));
-            } else {
-                postdto.setImage("https://images.unsplash.com/photo-1536765659537-ac6b544ea73b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
+                if(map.get("image") != null) {
+                    postdto.setImage((String) map.get("image"));
+                } else {
+                    postdto.setImage("https://images.unsplash.com/photo-1536765659537-ac6b544ea73b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
+                }
+                postdto.setTitle((String) map.get("title"));
+                postdto.setAreacode((int) map.get("areacode"));
+                postdto.setContent_id((int) map.get("content_id"));
+                postdto.setCat1((String) map.get("cat1"));
+                postdto.setCat2((String) map.get("cat2"));
+                postdto.setCat3((String) map.get("cat3"));
+                postdto.setContent_type_id((int) map.get("content_type_id"));
+                
+                if(map.get("mapx") != null && map.get("mapy") != null) {
+                    postdto.setMapx((double) map.get("mapx"));
+                    postdto.setMapy((double) map.get("mapy"));
+                } else {
+                    postdto.setMapx(0.0);
+                    postdto.setMapy(0.0);
+                }
+                testdao.addlocationBasedList(postdto);
             }
-            postdto.setTitle((String) map.get("title"));
-            postdto.setAreacode((int) map.get("areacode"));
-            postdto.setContent_id((int) map.get("content_id"));
-            postdto.setCat3((String) map.get("cat3"));
-            postdto.setContent_type_id((int) map.get("content_type_id"));
-            
-            if(map.get("mapx") != null && map.get("mapy") != null) {
-                postdto.setMapx((double) map.get("mapx"));
-                postdto.setMapy((double) map.get("mapy"));
-            } else {
-                postdto.setMapx(0.0);
-                postdto.setMapy(0.0);
-            }
-
-            testdao.addlocationBasedList(postdto);
-
         }
     }
 
-    public List<Map<String, Object>> insertInfo() throws Exception {
-        System.out.println("****************insertInfo**********************");
+    public List<Map<String, Object>> insertInfo(int n) throws Exception {
+        // System.out.println("****************insertInfo**********************");
 
         List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
         String base_url = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList?ServiceKey=";
         String key = "rbssLvuPaumnRlgRCPsgU5IeLlHAf5nHHGU8P3JVSYqJvgSFL8l%2FJbCYNE9zVd5Je%2BFoFlSBFo%2Fochd7h97a%2Fg%3D%3D";
-        URL url = new URL(base_url + key + "&areaCode=39&numOfRows=5000&pageNo=1&MobileOS=ETC&MobileApp=AppTest");
+        URL url = new URL(base_url + key + "&areaCode=" + n + "&numOfRows=5000&MobileOS=ETC&MobileApp=AppTest");
         System.out.println(url);
         XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
         factory.setNamespaceAware(true);
@@ -83,7 +87,7 @@ public class TestService {
         int event_type = xpp.getEventType();
 
         Map<String, Object> tempMap = null;
-       
+        
         while (event_type != XmlPullParser.END_DOCUMENT) {
             if (event_type == XmlPullParser.START_TAG) {
                 tag = xpp.getName();
@@ -102,6 +106,10 @@ public class TestService {
                     tempMap.put("areacode", Integer.parseInt(xpp.getText()));
                 } else if (tag.equals("contentid")) {
                     tempMap.put("content_id", Integer.parseInt(xpp.getText()));
+                } else if (tag.equals("cat1")) {
+                    tempMap.put("cat1", xpp.getText());
+                } else if (tag.equals("cat2")) {
+                    tempMap.put("cat2", xpp.getText());
                 } else if (tag.equals("cat3")) {
                     tempMap.put("cat3", xpp.getText());
                 } else if (tag.equals("contenttypeid")) {
@@ -110,8 +118,7 @@ public class TestService {
                     tempMap.put("mapx", Double.parseDouble(xpp.getText()));
                 } else if (tag.equals("mapy")) {
                     tempMap.put("mapy", Double.parseDouble(xpp.getText()));
-                } 
-
+                }
             } else if (event_type == XmlPullParser.END_TAG) {
                 tag = xpp.getName();
                 if (tag.equals("item")) {
