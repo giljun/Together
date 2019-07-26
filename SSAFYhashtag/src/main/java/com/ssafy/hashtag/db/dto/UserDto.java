22cdc751b5e468aafef421697e750387cbba379d
Lee heon gyu <frtt0608@naver.com>
2019-07-26 09:09:20 +0900

    190726|User table에 email컬럼 추가

--- SSAFYhashtag/src/main/java/com/ssafy/hashtag/db/dto/UserDto.java
+++ SSAFYhashtag/src/main/java/com/ssafy/hashtag/db/dto/UserDto.java
@@ -1,9 +1,10 @@
 package com.ssafy.hashtag.db.dto;
 
 public class UserDto {
-    
+
   private int user_pk;
   private String name;
+  private String email;
   private String nickname;
   private String phone_number;
   private String password;
@@ -15,95 +16,128 @@ public class UserDto {
   private String updated_at;
 
   public UserDto() {
-      super();
-      // TODO Auto-generated constructor stub
+    super();
+    // TODO Auto-generated constructor stub
   }
-  public UserDto(int user_pk, String name, String nickname, String phone_number, String password, int grade,
-          String profile_Imgae, int together, String description, String created_at, String updated_at) {
-      super();
-      this.user_pk = user_pk;
-      this.name = name;
-      this.nickname = nickname;
-      this.phone_number = phone_number;
-      this.password = password;
-      this.grade = grade;
-      this.profile_Imgae = profile_Imgae;
-      this.together = together;
-      this.description = description;
-      this.created_at = created_at;
-      this.updated_at = updated_at;
+
+  public UserDto(int user_pk, String name, String email, String nickname, String phone_number, String password,
+      int grade, String profile_Imgae, int together, String description, String created_at, String updated_at) {
+    super();
+    this.user_pk = user_pk;
+    this.name = name;
+    this.email = email;
+    this.nickname = nickname;
+    this.phone_number = phone_number;
+    this.password = password;
+    this.grade = grade;
+    this.profile_Imgae = profile_Imgae;
+    this.together = together;
+    this.description = description;
+    this.created_at = created_at;
+    this.updated_at = updated_at;
   }
+
   public int getUser_pk() {
-      return user_pk;
+    return user_pk;
   }
+
   public void setUser_pk(int user_pk) {
-      this.user_pk = user_pk;
+    this.user_pk = user_pk;
   }
+
   public String getName() {
-      return name;
+    return name;
   }
+
   public void setName(String name) {
-      this.name = name;
+    this.name = name;
   }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
   public String getNickname() {
-      return nickname;
+    return nickname;
   }
+
   public void setNickname(String nickname) {
-      this.nickname = nickname;
+    this.nickname = nickname;
   }
+
   public String getPhone_number() {
-      return phone_number;
+    return phone_number;
   }
+
   public void setPhone_number(String phone_number) {
-      this.phone_number = phone_number;
+    this.phone_number = phone_number;
   }
+
   public String getPassword() {
-      return password;
+    return password;
   }
+
   public void setPassword(String password) {
-      this.password = password;
+    this.password = password;
   }
+
   public int getGrade() {
-      return grade;
+    return grade;
   }
+
   public void setGrade(int grade) {
-      this.grade = grade;
+    this.grade = grade;
   }
+
   public String getProfile_Imgae() {
-      return profile_Imgae;
+    return profile_Imgae;
   }
+
   public void setProfile_Imgae(String profile_Imgae) {
-      this.profile_Imgae = profile_Imgae;
+    this.profile_Imgae = profile_Imgae;
   }
+
   public int getTogether() {
-      return together;
+    return together;
   }
+
   public void setTogether(int together) {
-      this.together = together;
+    this.together = together;
   }
+
   public String getDescription() {
-      return description;
+    return description;
   }
+
   public void setDescription(String description) {
-      this.description = description;
+    this.description = description;
   }
+
   public String getCreated_at() {
-      return created_at;
+    return created_at;
   }
+
   public void setCreated_at(String created_at) {
-      this.created_at = created_at;
+    this.created_at = created_at;
   }
+
   public String getUpdated_at() {
-      return updated_at;
+    return updated_at;
   }
+
   public void setUpdated_at(String updated_at) {
-      this.updated_at = updated_at;
+    this.updated_at = updated_at;
   }
+
   @Override
   public String toString() {
-      return "UserDto [user_pk=" + user_pk + ", name=" + name + ", nickname=" + nickname + ", phone_number="
-              + phone_number + ", password=" + password + ", grade=" + grade + ", profile_Imgae=" + profile_Imgae
-              + ", together=" + together + ", description=" + description + ", created_at=" + created_at
-              + ", updated_at=" + updated_at + "]";
+    return "UserDto [user_pk=" + user_pk + ", name=" + name + ", nickname=" + nickname + ", phone_number="
+        + phone_number + ", password=" + password + ", grade=" + grade + ", profile_Imgae=" + profile_Imgae
+        + ", together=" + together + ", description=" + description + ", created_at=" + created_at + ", updated_at="
+        + updated_at + "]";
   }
 }
