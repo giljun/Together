90bc09eda81cdf65592c9f4de5701b3ab99d3850
박종찬 <seheehan912@gmail.com>
2019-08-13 17:21:16 +0900

    fin

--- pjt_trip/src/views/schedule.vue
+++ pjt_trip/src/views/schedule.vue
@@ -5,8 +5,8 @@
     <v-container fluid>
       <v-layout wt text-center text-xs-center justify-center style="padding-bottom:5%">
         <div class="wt align-center justify-center text-center">
-          <p class="title">Schedule</p>
-          <p>스케줄을 관리하고 확인하세요</p>
+          <p style="font-size: 45px;">Schedule</p>
+          <p class="font">스케줄을 관리하고 확인하세요</p>
         </div>
       </v-layout>
       <v-layout>
@@ -282,5 +282,13 @@ export default {
 
 .layoutdiv {
   padding-top: 30%;
+}
+
+.font{
+  font-size:20px;
+}
+.title {
+ font-size: 50px;
+ font-weight: bold;
 }
 </style>
